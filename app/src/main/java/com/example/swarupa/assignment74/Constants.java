4ad6df0beb6d7383712d1dd22e076c619b4e7e60
Swarupa17 <swarupa.khapli@yahoo.in>
2016-12-07 14:24:31 +0530

    Initial commit

---
+++ app/src/main/java/com/example/swarupa/assignment74/Constants.java
@@ -0,0 +1,19 @@
+package com.example.swarupa.assignment74;
+
+/**
+ * Created by swarupa on 07-12-2016.
+ */
+
+public class Constants {
+
+    public static final int ADD_RECORD = 0;
+    public static final int UPDATE_RECORD = 1;
+    public static final String DML_TYPE = "DML_TYPE";
+    public static final String UPDATE = "Update";
+    public static final String INSERT = "Insert";
+    public static final String DELETE = "Delete";
+    public static final String FIRST_NAME = "Firstname";
+    public static final String LAST_NAME = "Lastname";
+    public static final String ID = "ID";
+
+}
---
+++ app/src/main/java/com/example/swarupa/assignment74/ContactModel.java
@@ -0,0 +1,34 @@
+package com.example.swarupa.assignment74;
+
+/**
+ * Created by swarupa on 07-12-2016.
+ */
+
+public class ContactModel {
+
+    private String ID, firstName, lastName;
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
